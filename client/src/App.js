6b7ff027b7918675e34fd7cef96bd8b9fc25cbac
Kyle Mulqueen <kmulqdrums710@gmail.com>
2023-07-14 23:27:19 -0700

    testing docker ci

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Test</header>
+      <header className="App-header">Testing...</header>
     </div>
   );
 }
